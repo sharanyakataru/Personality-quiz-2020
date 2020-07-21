d1aedfd07aa484f12d1f418d4ec5db5864f5b285
Glitch (personality-quiz-starter-code) <none>
2020-07-21 23:57:55 +0000

    🎗🏉 Checkpoint ./script.js:19016887/1078

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ var result1Score = 0;
 
  var You are a very outgoing and a comprehensive person = 0;
   var You are a very outgoing and critically thinking person= 0;
-  var  = 0;
-  var ravenclawScore = 0;
+  var You are a very reserved and critically thinking person = 0;
+  var You are a very reserved and a comprehensive person= 0;
 
 
 
@@ -15,6 +15,29 @@ var result1Score = 0;
 
 //#TODO: Use the DOM to create variables for the first quiz question.
 var q1a1 = document.getElementById("q1a1");
+var q1a2 = document.getElementById("q1a2");
+var q1a3 = document.getElementById("q1a3");
+var q1a4 = document.getElementById("q1a4");
+
+var q1a1 = document.getElementById("q2a1");
+var q1a2 = document.getElementById("q2a2");
+var q1a3 = document.getElementById("q2a3");
+var q1a4 = document.getElementById("q2a4");
+
+var q1a1 = document.getElementById("q3a1");
+var q1a2 = document.getElementById("q3a2");
+var q1a3 = document.getElementById("q3a3");
+var q1a4 = document.getElementById("q3a4");
+
+var q1a1 = document.getElementById("q4a1");
+var q1a2 = document.getElementById("q4a2");
+var q1a3 = document.getElementById("q4a3");
+var q1a4 = document.getElementById("q4a4");
+
+var q1a1 = document.getElementById("q5a1");
+var q1a2 = document.getElementById("q5a2");
+var q1a3 = document.getElementById("q5a3");
+var q1a4 = document.getElementById("q5a4");
 
 
 

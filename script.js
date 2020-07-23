8f142128eabd1f7d13af3061999aeac307d8015c
Glitch (personality-quiz-starter-code) <none>
2020-07-23 19:07:59 +0000

    🏊🏼🎀 Checkpoint ./script.js:19016887/29

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var result3Score = 0;
 var result4Score = 0;
 
  // var result1 You are a very outgoing and a comprehensive person = 0;
- //  var result 2 ou are a very outgoing and critically thinking person= 0;
+ //  var result 2 You are a very outgoing and critically thinking person= 0;
  //  var result3 You are a very reserved and critically thinking person = 0;
  //  var result4You are a very reserved and a comprehensive person= 0;
 
@@ -35,20 +35,20 @@ var q2a2 = document.getElementById("q2a2");
 var q2a3 = document.getElementById("q2a3");
 var q2a4 = document.getElementById("q2a4");
 
-var q1a1 = document.getElementById("q3a1");
-var q1a2 = document.getElementById("q3a2");
-var q1a3 = document.getElementById("q3a3");
-var q1a4 = document.getElementById("q3a4");
+var q3a1 = document.getElementById("q3a1");
+var q3a2 = document.getElementById("q3a2");
+var q3a3 = document.getElementById("q3a3");
+var q3a4 = document.getElementById("q3a4");
 
-var q1a1 = document.getElementById("q4a1");
-var q1a2 = document.getElementById("q4a2");
-var q1a3 = document.getElementById("q4a3");
-var q1a4 = document.getElementById("q4a4");
+var q4a1 = document.getElementById("q4a1");
+var q4a2 = document.getElementById("q4a2");
+var q4a3 = document.getElementById("q4a3");
+var q4a4 = document.getElementById("q4a4");
 
-var q1a1 = document.getElementById("q5a1");
-var q1a2 = document.getElementById("q5a2");
-var q1a3 = document.getElementById("q5a3");
-var q1a4 = document.getElementById("q5a4");
+var q5a1 = document.getElementById("q5a1");
+var q5a2 = document.getElementById("q5a2");
+var q5a3 = document.getElementById("q5a3");
+var q5a4 = document.getElementById("q5a4");
 
 
 
